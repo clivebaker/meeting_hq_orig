80eceb24744e7a9a96a6d7c16dec661cd341684b
Clive Baker <clive@clivebaker.com>
2022-01-19 16:33:45 +0000

    wip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+
+
+  def nav_active?(controller)
+
+    "active" if params[:controller] == controller
+
+  end
+  
+
+  def nav_includes?(controllers) 
+    
+    ret = {collapse: 'collapse', active: ''}
+    
+    ret = {collapse: '', active: 'active'} if controllers.include?(params[:controller])
+
+    ret
+
+  end
+  
+
+
+  
 end
