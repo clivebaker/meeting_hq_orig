1480d2c5acd003f41a4a546aecbcfec2578422e9
Clive Baker <clive@clivebaker.com>
2022-03-10 20:26:54 +0000

    test

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,39 +3,31 @@
 
 
 
-require("@hotwired/turbo-rails");
-require("./controllers");
-JQuery = require("jquery");
-
-//import JQuery from 'jquery';
+import "@hotwired/turbo-rails";
+import "./controllers";
+//require("jquery");
+import JQuery from 'jquery';
 window.$ = window.JQuery = JQuery;
 
-require("datatables.net");
-require("datatables.net-buttons");
-require("datatables.net-buttons/js/buttons.html5.js");
-require("datatables.net-buttons/js/buttons.print.js");
-require("datatables.net-buttons-bs5");
-require("datatables.net-responsive");
-require("datatables.net-select");
-require("datatables.net-fixedheader");
+import "datatables.net";
+import "datatables.net-buttons";
+import "datatables.net-buttons/js/buttons.html5.js";
+import "datatables.net-buttons/js/buttons.print.js";
+import "datatables.net-buttons-bs5";
+import "datatables.net-responsive";
+import "datatables.net-select";
+import "datatables.net-fixedheader";
 
-
-//import DataTable from 'datatables.net-bs5';
-
-DataTable = require('datatables.net-bs5');
+import DataTable from 'datatables.net-bs5';
 window.DataTable = DataTable();
 
-Bootstrap = require('bootstrap');
-//import * as bootstrap from 'bootstrap';
-window.bootstrap = bootstrap;
-
-
-require("./modules/theme");
-// require("./modules/dragula");
-// require("./modules/feather");
-require("./modules/moment");
-require("./modules/sidebar");
-require("./modules/notyf");
+import "./modules/bootstrap";
+import "./modules/theme";
+// import "./modules/dragula";
+// import "./modules/feather";
+import "./modules/moment";
+import "./modules/sidebar";
+import "./modules/notyf";
 
 
 
